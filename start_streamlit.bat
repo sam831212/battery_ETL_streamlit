f3d5503e9a94eeebd40dffe99c153bec8dbb666d
sam831212 <35685140+sam831212@users.noreply.github.com>
2025-09-18 14:44:25 +0800

    Update start_streamlit.bat

--- start_streamlit.bat
+++ start_streamlit.bat
@@ -1,5 +1,1 @@
-﻿@echo off
-REM 啟動 Streamlit 並自動開啟瀏覽器到 http://localhost:5000
 
-REM 啟動 Streamlit 並監聽所有網絡接口，端口設置為 8501
-streamlit run main.py --server.port 8501 --server.address 0.0.0.0 --server.headless true
